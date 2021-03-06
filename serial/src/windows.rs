99c9d6b9277c12372cc4a949d7ea74565408fcb4
Michael Farrell <micolous+git@gmail.com>
2021-03-06 22:55:42 +1100

    windows: major memory cleanup

--- serial/src/windows.rs
+++ serial/src/windows.rs
@@ -14,33 +14,19 @@ use winapi::um::coml2api::STGM_READ;
 use winapi::um::combaseapi::{CoCreateInstance};
 use winapi::um::mmdeviceapi::{CLSID_MMDeviceEnumerator, IMMDevice, IMMDeviceEnumerator, eCapture, eCommunications};
 use winapi::um::objbase::CoInitialize;
-use winapi::um::propidl::PROPVARIANT;
 use winapi::um::propsys::IPropertyStore;
 use winapi::um::functiondiscoverykeys_devpkey::PKEY_Device_FriendlyName;
 use winapi::um::propkeydef::REFPROPERTYKEY;
 use winapi::um::winbase::lstrlenW;
-use winapi::shared::ntdef::HRESULT;
 use winapi::shared::wtypesbase::CLSCTX_INPROC_SERVER;
 use winapi::Interface;
 
-// https://gist.github.com/DrMetallius/084115493deb21148a6bef9326b47ea6
 macro_rules! try_com {
-	($expr:expr) => (if $expr != 0 {
+	($expr:expr) => (
+        if $expr != 0 {
 			return Err(format!("HRESULT: 0x{:X}", $expr))
-		})
-}
-
-macro_rules! new_ref {
-//	($param:ident, $type:ty) => {let mut $param: &mut $type = mem::uninitialized(); }
-    ($param:ident, $type:ty) => {let mut $param: *mut $type = mem::zeroed(); }
-//	($param:ident, $type:ty) => {let mut $param = mem::MaybeUninit::<*mut $type >::uninit(); }
-}
-
-macro_rules! out_param {
-//    ($param:ident) => {&mut $param as *mut _ as *mut _}
-//    ($param:ident) => {&mut $param.as_mut_ptr() as *mut _ as *mut _}
-//    ($param:ident) => {&mut $param.as_mut_ptr() as *mut *mut _ as *mut *mut _}
-    ($param:ident) => {&mut $param as *mut *mut _ as *mut *mut _}
+		}
+    )
 }
 
 pub struct WindowsAudioController {
@@ -56,40 +42,26 @@ pub struct WindowsAudioInputDevice {
 impl WindowsAudioController {   
     fn get_device_enumerator(&self) -> Result<*mut IMMDeviceEnumerator, String> {
         unsafe {
-            new_ref!(device_enumerator, IMMDeviceEnumerator);
+            let mut device_enumerator = mem::MaybeUninit::<&mut IMMDeviceEnumerator>::uninit();
             try_com!(CoCreateInstance(
                 &CLSID_MMDeviceEnumerator,
                 null_mut(),
                 CLSCTX_INPROC_SERVER,
                 &IMMDeviceEnumerator::uuidof() as *const _,
-                out_param!(device_enumerator)));
-//            Ok(device_enumerator.assume_init())
-            Ok(device_enumerator)
+                device_enumerator.as_mut_ptr() as *mut *mut _));
+            Ok(device_enumerator.assume_init())
         }
     }
-    
-    /*
-    fn enum_audio_endpoints(&self, device_enumerator: *mut IMMDeviceEnumerator) -> Result<*mut IMMDeviceCollection, String> {
-        unsafe {
-            new_ref!(device_collection, IMMDeviceCollection);
-            try_com!((*device_enumerator).EnumAudioEndpoints(
-                eRender,
-                DEVICE_STATE_ACTIVE,
-                out_param!(device_collection)));
-//            Ok(device_collection.assume_init())
-            Ok(device_collection)
-        }
-    }
-    */
-    
+
     fn get_default_communications_imm_device(&self, device_enumerator: *mut IMMDeviceEnumerator) -> Result<*mut IMMDevice, String> {
         unsafe {
-            new_ref!(mm_device, IMMDevice);
+            let mut mm_device = mem::MaybeUninit::<>::uninit();
+
             try_com!((*device_enumerator).GetDefaultAudioEndpoint(
                 eCapture,
                 eCommunications,
-                out_param!(mm_device)));
-            Ok(mm_device)
+                mm_device.as_mut_ptr()));
+            Ok(mm_device.assume_init())
         }
     }
 }
@@ -123,19 +95,20 @@ impl WindowsAudioInputDevice {
     
     fn open_property_store(mm_device: *mut IMMDevice) -> Result<*mut IPropertyStore, String> {
         unsafe {
-            new_ref!(props, IPropertyStore);
+            let mut props = mem::MaybeUninit::<>::uninit();
             try_com!((*mm_device).OpenPropertyStore(
                 STGM_READ,
-                &mut props));
-            Ok(props)
+                props.as_mut_ptr()));
+            Ok(props.assume_init())
         }
     }
     
     fn get_property_value(props: *mut IPropertyStore, key: REFPROPERTYKEY) -> Result<String, String> {
         unsafe {
-            let mut variant = mem::uninitialized();
-            // new_ref!(var_name, PROPVARIANT);
-            try_com!((*props).GetValue(key, &mut variant));
+            let mut variant = mem::MaybeUninit::<>::uninit();
+            try_com!((*props).GetValue(key, variant.as_mut_ptr()));
+            let variant = variant.assume_init();
+
             Ok(OsString::from_wide(
                 from_ptr(*variant.data.pwszVal())
             ).to_string_lossy().into_owned())
