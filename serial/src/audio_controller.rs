2c38a34d48163d245f61806ec49f36801c89121f
Michael Farrell <micolous+git@gmail.com>
2021-03-18 10:33:46 +1000

    cleanup audio controller

--- serial/src/audio_controller.rs
+++ serial/src/audio_controller.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Debug, Formatter};
 use std::result::Result;
 
 #[derive(Debug)]
@@ -7,22 +7,24 @@ pub struct AudioError {
     pub msg: String,
 }
 
+/// Trait that describes an audio input device.
 pub trait AudioInputDeviceTrait {
+    /// The human-readable name of the audio device.
     fn name(&self) -> Result<String, AudioError>;
+
+    /// Sets the mute state of the audio device.
     fn set_mute(&self, state: bool) -> Result<bool, AudioError>;
 }
 
+/// Trait that describes the audio subsystem.
 pub trait AudioControllerTrait {
+    /// Create a new connection to the audio subsystem.
     fn new() -> Box<dyn AudioControllerTrait>
     where
         Self: Sized;
-    fn get_comms_device(&self) -> Result<Box<dyn AudioInputDeviceTrait>, AudioError>;
-}
 
-impl Display for dyn AudioInputDeviceTrait {
-    fn fmt(&self, _: &mut Formatter<'_>) -> Result<(), fmt::Error> {
-        todo!()
-    }
+    /// Gets the default communications device.
+    fn get_comms_device(&self) -> Result<Box<dyn AudioInputDeviceTrait>, AudioError>;
 }
 
 impl Debug for dyn AudioInputDeviceTrait {
